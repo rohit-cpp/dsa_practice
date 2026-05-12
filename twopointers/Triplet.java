2cf4a6cefb2e6c9fc8da24b2fb782b326666b339
Rohit <rrgawanderohit@gmail.com>
2026-05-12 15:57:04 +0530

    Triplets duplicates sum

--- twopointers/Triplet.java
+++ twopointers/Triplet.java
@@ -10,7 +10,18 @@
 // Optimised solution for this. 
 // we have to find sum of three numbers whose sum is 0;
 //means x+y+z=0; means a[i]+a[i]+a[i] = 0; so keep two a[i] as x and y. so now the equation becomes a[i] + x + y = 0; or x+y = -a[i]
-
+// ok now for the unique elements in the array.(Two sum)
+// take two pointers left and right. left[i] right[n-1]
+// if sum of these both left[i] and right[n-1] = target
+// then left[i]++ and right[i]++
+//while(a[left]==a[left-1])
+//left++
+//while(a[right]==a[right+1])
+//right--
+//else if (sum<target)
+//left++
+//else if (sum>target)
+//right--
 import java.util.*;
 
 class Triplet {
@@ -31,6 +42,7 @@ class Triplet {
                 continue;
             }
 
+            // declare two pointers
             int left = i + 1;
             int right = n - 1;
 
@@ -80,19 +92,19 @@ class Triplet {
 
     public static void main(String[] args) {
         Triplet solution = new Triplet();
-        
+
         // Test case 1
-        int[] nums1 = {-1, 0, 1, 2, -1, -4};
+        int[] nums1 = { -1, 0, 1, 2, -1, -4 };
         System.out.println("Test case 1: " + Arrays.toString(nums1));
         System.out.println("Result: " + solution.threeSum(nums1));
-        
+
         // Test case 2
-        int[] nums2 = {0, 0, 0};
+        int[] nums2 = { 0, 0, 0 };
         System.out.println("\nTest case 2: " + Arrays.toString(nums2));
         System.out.println("Result: " + solution.threeSum(nums2));
-        
+
         // Test case 3
-        int[] nums3 = {-2, 0, 1, 1, 2};
+        int[] nums3 = { -2, 0, 1, 1, 2 };
         System.out.println("\nTest case 3: " + Arrays.toString(nums3));
         System.out.println("Result: " + solution.threeSum(nums3));
     }
