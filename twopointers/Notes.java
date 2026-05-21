d6a0f515049d8a7a0f6750bfcd2c363728d03bb2
Rohit <rrgawanderohit@gmail.com>
2026-05-21 08:39:19 +0530

    added sliding window

--- twopointers/Notes.java
+++ twopointers/Notes.java
@@ -147,3 +147,30 @@
 // }
 // return true;
 // }
+
+class Notes {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else if (nums[left] + nums[right] < target) {
+                right--;
+            }
+        }
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 7, 11, 15 };
+        int target = 9;
+        int result[] = twoSum(numbers, target);
+        System.out.println("result :" + result);
+    }
+}
--- twopointers/TwoPointer.java
+++ twopointers/TwoPointer.java
@@ -273,3 +273,32 @@
 // System.out.println("Reverse String is : " + result);
 // }
 // }
+
+import java.util.Arrays;
+
+class TwoPointer {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 7, 11, 15 };
+        int target = 9;
+        int result[] = twoSum(numbers, target);
+        System.out.println("result :" + Arrays.toString(result));
+    }
+}
