ebc92794197701a9b99eb5c87815bffda1298024
Rohit <rrgawanderohit@gmail.com>
2026-05-21 09:45:19 +0530

    added the concept of the sliding window

--- slidingWindow/new.java
+++ slidingWindow/new.java
@@ -1,5 +1,27 @@
-package slidingWindow;
+// package slidingWindow;
 
-public class new {
-    
-}
+// public class new {
+
+// }
+
+// It will be in Array and Strings only it will not be in the linkd list.
+// if in question has subarray or substring then it will be sliding window.
+
+// subarray means continuos
+// subsequence means non-continus
+
+// question want us to find -
+// maximum,minimum,longest,shortest,sum/count/average,at most k/at least k/
+// exactly k.
+
+// this to be looked only when the upper one is compatible. SO if all those
+// conditions are satisfied then onyl we will do sliding window.
+
+// ONE MORE THING.
+// sliding window has two types.
+// fixed and dynamic
+
+// sometimes the size or lenght is given of subarray or substring.
+// Then we need to take fixed window
+
+// and if the size or length is not given then we need to take dynamic.
